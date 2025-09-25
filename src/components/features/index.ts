d60a87a0f87783e3998c6b74a9e60c860a93b2fe
glafay <guillaumel1103@gmail.com>
2025-09-25 14:18:29 +0200

    good

---
+++ src/components/features/index.ts
@@ -0,0 +1,8 @@
+export { default as Fonctionnement } from './Fonctionnement';
+export { default as FAQ } from './FAQ';
+export { default as Amis } from './amis';
+export { default as Price } from './price';
+export { default as QRCodeSection } from './QRCodeSection';
+export { default as Securite } from './securité';
+
+
